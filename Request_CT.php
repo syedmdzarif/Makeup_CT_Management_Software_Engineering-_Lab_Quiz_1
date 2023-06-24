405d7e1614326b05fa22b7bee759491f9cf14b4c
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-06-24 14:58:19 +0600

    commit for request insert frontend form

--- Request_CT.php
+++ Request_CT.php
@@ -1,7 +1,17 @@
-<h1>Create Record</h1>
+<h1>Create a request</h1>
 
 <form method="GET" action="Request_entry.php">
 
+	Name: <input type="text" name="name"> 
+    <br>
+    Missed Course: <input type="text" name="missed_course"> 
+    <br>
+    CT Date: <input type="text" name="ct_date"> 
+    <br>
+    Reason: <input type="text" name="reason"> 
+    <br>
+
+   
 
 </form>
 
