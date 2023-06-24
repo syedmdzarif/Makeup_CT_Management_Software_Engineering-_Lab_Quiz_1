e99084d7d63a6b4cc4be858dbd43119b4d3c0537
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-06-24 14:55:52 +0600

    commit for request insert frontend

--- Request_CT.php
+++ Request_CT.php
@@ -1,17 +1,7 @@
 <h1>Create Record</h1>
 
-<form method="GET" action="booking.php">
+<form method="GET" action="Request_entry.php">
 
-	Name: <input type="text" name="name"> 
-    <br>
-    Date: <input type="text" name="date"> 
-    <br>
-    From: <input type="text" name="from"> 
-    <br>
-    To: <input type="text" name="to"> 
-    <br>
-
-    <input type="submit">
 
 </form>
 
